d42dda6cbc1ca3f88bc4fff3d3398734c4779860
Chris Sakundiak <chris.sakundiak@gmail.com>
2024-09-15 21:35:52 -0700

    Commit 3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] int moveSpeed = 1;
     [SerializeField] Rigidbody2D playerRigidBody;
+    [SerializeField] Animator playerAnimator;
     
     void Start()
     {
         
     }
 
-    
+
     void Update()
     {
         float horizontalMovement = Input.GetAxisRaw("Horizontal");
         float verticalMovement = Input.GetAxisRaw("Vertical");
-        playerRigidBody.velocity = new Vector2(horizontalMovement, verticalMovement);
+
+        playerRigidBody.velocity = new Vector2(horizontalMovement, verticalMovement) * moveSpeed;
+
+        playerAnimator.SetFloat("movementX", playerRigidBody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidBody.velocity.y);
+
+        if (horizontalMovement == 1 || horizontalMovement == -1 || verticalMovement == 1 || verticalMovement == -1)
+        {
+            playerAnimator.SetFloat("lastX", horizontalMovement);
+            playerAnimator.SetFloat("lastY", verticalMovement);
+        }   
     }
 }
